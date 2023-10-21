d2ffe08a62f1dce06c3d2b30902e2ca5fa15fe81
Yurii Kiktenko <yuri@kik10.com>
2023-10-21 12:55:02 +0200

    MongoDB connection setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
 import express, { Request, Response, Application } from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDefinition from './swagger';
 
 const app: Application = express();
 const PORT = 3000;
+
+const uri: string = "mongodb+srv://epamhw:epamnode@epam.ovtqb1a.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (err) {
+    console.dir(err);
+  } finally {
+    await client.close();
+  }
+}
+
+run();
 
 const swaggerDocs = swaggerJsDoc({
   definition: swaggerDefinition,
